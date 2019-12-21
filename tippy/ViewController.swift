f7408526dd5467fd4f653768b215248c7a165409
Pavel Savva <pashasavva@gmail.com>
2019-12-20 19:54:47 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Pavel Savva on 12/20/19.
+//  Copyright © 2019 Pavel Savva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
