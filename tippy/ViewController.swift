6e37360c10a090cb188af64f3a9b874c6b0551ff
Pavel Savva <pashasavva@gmail.com>
2019-12-22 19:27:47 -0800

    Using locale-specific currency and currency thousands separators. Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
@@ -18,21 +18,35 @@ class ViewController: UIViewController {
     private var selectedTipPercentage: Double = 0.0
     public static var tipPercentages: [Double] = [0.15, 0.18, 0.2]
     
+    
     override func viewWillAppear(_ animated: Bool) {
         
-       // Sets the title in the Navigation Bar
-       self.title = "Tip Calculator"
-        
         let defaults = UserDefaults.standard
-
-        selectedTipPercentage = defaults.double(forKey: "defaultTipPercentage");
         
+        // Sets the title in the Navigation Bar
+        self.title = "Tip Calculator"
+        
+        // Get saved default for tip percentage
+        selectedTipPercentage = defaults.double(forKey: "defaultTipPercentage");
         tipControl.selectedSegmentIndex = ViewController.tipPercentages.firstIndex(of: selectedTipPercentage) ?? 0;
-
+        
+        // Get last bill
+        let lastBill = defaults.double(forKey: "lastBill")
+        let lastBillDate = defaults.array(forKey: "lastBillDate")  as? [Date] ?? [Date()]
+        if(lastBill != 0 && Date().timeIntervalSince(lastBillDate[0]) < 10 * 60 * 1000) {
+            billField.text = String(format: "%g", lastBill)
+        }
+        
         calculateTipLogic()
-       // ...
+        keepFirstResponder()
     }
-
+    
+    func keepFirstResponder() {
+        DispatchQueue.main.async {
+            self.billField.becomeFirstResponder()
+        }
+    }
+    
     @IBAction func onTap(_ sender: Any) {
         
         view.endEditing(true)
@@ -42,17 +56,42 @@ class ViewController: UIViewController {
         calculateTipLogic()
     }
     
+    @IBAction func tap(_ sender: Any) {
+        keepFirstResponder()
+    }
+    
+    func convertToLocalCurrency(tip: Double, total: Double) -> (String, String) {
+        let locale = Locale.current
+
+        // Convert
+        let fmt = NumberFormatter()
+        fmt.numberStyle = .currency
+        fmt.locale = locale
+        let tipString = fmt.string(from: NSNumber(value: tip)) ?? "$0.00"
+        let totalString = fmt.string(from: NSNumber(value: total)) ?? "$0.00"
+        
+        return (tipString, totalString)
+    }
+    
     func calculateTipLogic() {
+        let defaults = UserDefaults.standard
+        
         // Get the bill amount
         let bill = Double(billField.text!) ?? 0
         // Calculate the tip and total
         
+        
         let tip = bill * selectedTipPercentage;
         let total = bill + tip
         
+        let tipTotalStrings = convertToLocalCurrency(tip: tip, total: total)
+        
         // Update the tip and total balance
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        tipLabel.text = tipTotalStrings.0
+        totalLabel.text = tipTotalStrings.1
+        
+        // Force UserDefaults to save.
+        defaults.synchronize()
     }
     
     @IBAction func changeTipPercentage(_ sender: Any) {
